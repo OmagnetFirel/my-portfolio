1aed955ef78ee7b5c93160a69d65ac548130bc98
Victor Mouza <victor.mouza@agenciam3.com>
2022-07-05 21:30:31 -0300

    feat: configura deploy serve

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("uwu");
+const express = require('express');
+const {
+    resolve
+} = require('path')
+const app = express();
+
+app.use('/', 
+    express.static(resolve(__dirname, './build'))
+    )
+
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) return console.log(err);
+    return console.log("tudo funcionando")
+})
