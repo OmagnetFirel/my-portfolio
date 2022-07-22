a78728559be98f1bcf5c60bffef2166e178439dc
Victor Mouza <victor.mouza@agenciam3.com>
2022-07-22 12:22:50 -0300

    feat: sla

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { UniversityBlog } from "./routes/university-blog";
 import { BlogSelva } from "./routes/blog-selva";
 import { P1FundWeb } from "./routes/p1-fundweb";
@@ -17,14 +17,16 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/universityblog" element={<UniversityBlog />} />
-        <Route path="/blog-selva" element={<BlogSelva />} />
-        <Route path="/p1-fundweb" element={<P1FundWeb />} />
-        <Route path="/supertodo" element={<SuperTodo />} />
-        <Route path="/signup" element={<SignUpWrapper />} />
-      </Routes>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/universityblog" element={<UniversityBlog />} />
+          <Route path="/blog-selva" element={<BlogSelva />} />
+          <Route path="/p1-fundweb" element={<P1FundWeb />} />
+          <Route path="/supertodo" element={<SuperTodo />} />
+          <Route path="/signup" element={<SignUpWrapper />} />
+        </Routes>
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
